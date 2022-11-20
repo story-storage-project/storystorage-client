6b889349b69a8a3f00565c813589964198789786
임현정 | Alex Im <84281505+h-alex2@users.noreply.github.com>
2022-11-20 21:11:35 +0900

    Feat 코드 하이라이트 추가 커스텀 훅

---
+++ src/hooks/useCssHighLightQueryText.jsx
@@ -0,0 +1,107 @@
+import React, { useCallback, useRef, useState } from 'react';
+import styled from 'styled-components';
+import {
+  cssKeyOfcssProperties,
+  jsxKeyOfCssProperties,
+} from '../data/cssJsxProperties';
+import keyMaker from '../utils/keyMaker';
+
+export default function useCssHighLightQueryText(cssCode) {
+  const copyCssCode = useRef(cssCode);
+  const highlightElement = useRef();
+
+  const query = useCallback(
+    code => {
+      copyCssCode.current = code;
+      const bracketSplit = code.split(/(<!--|{|-->|}|;|:)/);
+      let isInTag = false;
+
+      const element = bracketSplit.map((item, i) => {
+        if (bracketSplit[i - 1] === '<!--' && bracketSplit[i + 1] === '-->') {
+          return <Punctuati key={keyMaker(10) + item}>{item}</Punctuati>;
+        }
+
+        if (item === '{') {
+          isInTag = true;
+          return <Punctuati key={keyMaker(10) + item}>{item}</Punctuati>;
+        }
+
+        if (
+          (isInTag && bracketSplit[i - 1] === '{') ||
+          bracketSplit[i - 1] === ';'
+        ) {
+          const removeEscapeChr = item.replace(/\n| |\t/g, '');
+          const isCssProperty =
+            cssKeyOfcssProperties[removeEscapeChr] ||
+            jsxKeyOfCssProperties[removeEscapeChr];
+          return isCssProperty ? (
+            <TagCode key={keyMaker(10) + item}>{item}</TagCode>
+          ) : (
+            <IncorrectTagCode key={keyMaker(10) + item}>
+              {item}
+            </IncorrectTagCode>
+          );
+        }
+
+        if (isInTag && item === ':') {
+          return <Punctuati key={keyMaker(10) + item}>{item}</Punctuati>;
+        }
+
+        if (isInTag && bracketSplit[i - 1] === ':') {
+          return bracketSplit[i + 1] === ';' ? (
+            <AttributeName key={keyMaker(10) + item}>{item}</AttributeName>
+          ) : (
+            <InCorrectAttributeName key={keyMaker(10) + item}>
+              {item}
+            </InCorrectAttributeName>
+          );
+        }
+
+        if (item === '}') {
+          isInTag = false;
+          return <Punctuati key={keyMaker(10) + item}>{item}</Punctuati>;
+        }
+
+        return item;
+      });
+
+      highlightElement.current = element;
+    },
+    [cssCode],
+  );
+
+  return [copyCssCode.current, highlightElement.current, query];
+}
+
+const Code = styled.span`
+  max-width: inherit;
+  overflow-wrap: break-word;
+  white-space: break-spaces;
+  word-wrap: break-word;
+  word-break: break-all;
+  box-sizing: border-box;
+`;
+
+const TagCode = styled(Code)`
+  max-width: inherit;
+  word-break: break-all;
+  color: ${props => props.theme.codeThemeBright.tagColor};
+`;
+
+const IncorrectTagCode = styled(TagCode)`
+  text-decoration: underline dotted;
+`;
+
+const Punctuati = styled(Code)`
+  max-width: inherit;
+  word-break: break-all;
+  color: ${props => props.theme.codeThemeBright.punctuation};
+`;
+
+const AttributeName = styled(Code)`
+  color: ${props => props.theme.codeThemeBright.attributeName};
+`;
+
+const InCorrectAttributeName = styled(AttributeName)`
+  text-decoration: underline dotted;
+`;
---
+++ src/hooks/useHtmlHighLightQueryText.jsx
@@ -0,0 +1,122 @@
+import React, { Fragment, useState, useRef, useCallback } from 'react';
+import styled from 'styled-components';
+import htmlElementsSet from '../data/htmlElements';
+import {
+  htmlKeyOfhtmlJsxAttributes,
+  jsxKeyOfhtmlJsxAttributes,
+} from '../data/htmlJsxAttributes';
+import keyMaker from '../utils/keyMaker';
+
+export default function useHtmlHighLightQueryText(htmlCode) {
+  const copyHtmlCode = useRef(htmlCode);
+  const highlightElement = useRef();
+
+  const query = useCallback(code => {
+    copyHtmlCode.current = code;
+    const bracketSplitCode = code.split(/(<!--| |-->|<|>)/);
+    let tagName = '';
+
+    const element = bracketSplitCode.map((item, i) => {
+      if (
+        bracketSplitCode[i - 1] === '<!--' &&
+        bracketSplitCode[i + 1] === '-->'
+      ) {
+        return <Punctuati key={keyMaker(10) + item}>{item}</Punctuati>;
+      }
+
+      if (item === '<') {
+        tagName = bracketSplitCode[i + 1];
+        return <Punctuati key={keyMaker(10) + item}>{item}</Punctuati>;
+      }
+
+      if (bracketSplitCode[i - 1] === '<') {
+        const itemData = item[0] === '/' ? item.slice(1) : item;
+        const isHtmlElement = htmlElementsSet.has(itemData);
+
+        return isHtmlElement ? (
+          <TagCode key={keyMaker(10) + item}>{item}</TagCode>
+        ) : (
+          <IncorrectTagCode key={keyMaker(10) + item}>{item}</IncorrectTagCode>
+        );
+      }
+
+      if (tagName && item !== '>') {
+        const splitAttribute = item.split(/(=)/);
+        const equalLength = splitAttribute.filter(itme => itme === '=').length;
+
+        if (equalLength > 1) {
+          return (
+            <AttributeValue key={keyMaker(10) + item}>{item}</AttributeValue>
+          );
+        }
+
+        const attributeName = splitAttribute[0];
+        const isCorrectAttributeName =
+          htmlKeyOfhtmlJsxAttributes[attributeName] ||
+          jsxKeyOfhtmlJsxAttributes[attributeName];
+
+        return isCorrectAttributeName ? (
+          <Fragment key={keyMaker(10) + item}>
+            <AttributeName>{splitAttribute[0]}</AttributeName>
+            <Punctuati>{splitAttribute[1]}</Punctuati>
+            <AttributeValue>{splitAttribute[2]}</AttributeValue>
+          </Fragment>
+        ) : (
+          <Fragment key={keyMaker(10) + item}>
+            <InCorrectAttributeName>{splitAttribute[0]}</InCorrectAttributeName>
+            <Punctuati>{splitAttribute[1]}</Punctuati>
+            <AttributeValue>{splitAttribute[2]}</AttributeValue>
+          </Fragment>
+        );
+      }
+
+      if (item === '>') {
+        tagName = '';
+        return <Punctuati key={keyMaker(10) + item}>{item}</Punctuati>;
+      }
+
+      return item;
+    });
+
+    highlightElement.current = element;
+  }, []);
+
+  return [copyHtmlCode.current, highlightElement.current, query];
+}
+
+const Code = styled.span`
+  max-width: inherit;
+  overflow-wrap: break-word;
+  white-space: break-spaces;
+  word-wrap: break-word;
+  word-break: break-all;
+  box-sizing: border-box;
+`;
+
+const TagCode = styled(Code)`
+  max-width: inherit;
+  word-break: break-all;
+  color: ${props => props.theme.codeThemeBright.tagColor};
+`;
+
+const IncorrectTagCode = styled(TagCode)`
+  text-decoration: underline dotted;
+`;
+
+const Punctuati = styled(Code)`
+  max-width: inherit;
+  word-break: break-all;
+  color: ${props => props.theme.codeThemeBright.punctuation};
+`;
+
+const AttributeName = styled(Code)`
+  color: ${props => props.theme.codeThemeBright.attributeName};
+`;
+
+const InCorrectAttributeName = styled(AttributeName)`
+  text-decoration: underline dotted;
+`;
+
+const AttributeValue = styled(Code)`
+  color: ${props => props.theme.codeThemeBright.attributeValue};
+`;
