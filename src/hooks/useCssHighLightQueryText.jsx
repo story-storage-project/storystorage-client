53380b82dd9a0f8ba0234397be9ab355e0a2edfc
임현정 | Alex Im <84281505+h-alex2@users.noreply.github.com>
2022-11-21 21:55:18 +0900

    Chore Ref 렌더링 안되는 이슈 때문에 state 생성

--- src/hooks/useCssHighLightQueryText.jsx
+++ src/hooks/useCssHighLightQueryText.jsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useRef, useState } from 'react';
+import React, { useCallback, useEffect, useRef, useState } from 'react';
 import styled from 'styled-components';
 import {
   cssKeyOfcssProperties,
@@ -8,67 +8,68 @@ import keyMaker from '../utils/keyMaker';
 
 export default function useCssHighLightQueryText(cssCode) {
   const copyCssCode = useRef(cssCode);
-  const highlightElement = useRef();
+  const highlightElement = useRef(cssCode);
+  const [, setRender] = useState(cssCode);
 
-  const query = useCallback(
-    code => {
-      copyCssCode.current = code;
-      const bracketSplit = code.split(/(<!--|{|-->|}|;|:)/);
-      let isInTag = false;
+  const query = useCallback(code => {
+    copyCssCode.current = code;
+    const bracketSplit = code.split(/(<!--|{|-->|}|;|:)/);
+    let isInTag = false;
 
-      const element = bracketSplit.map((item, i) => {
-        if (bracketSplit[i - 1] === '<!--' && bracketSplit[i + 1] === '-->') {
-          return <Punctuati key={keyMaker(10) + item}>{item}</Punctuati>;
-        }
+    const element = bracketSplit.map((item, i) => {
+      if (bracketSplit[i - 1] === '<!--' && bracketSplit[i + 1] === '-->') {
+        return <Punctuati key={keyMaker(10) + item}>{item}</Punctuati>;
+      }
 
-        if (item === '{') {
-          isInTag = true;
-          return <Punctuati key={keyMaker(10) + item}>{item}</Punctuati>;
-        }
+      if (item === '{') {
+        isInTag = true;
+        return <Punctuati key={keyMaker(10) + item}>{item}</Punctuati>;
+      }
 
-        if (
-          (isInTag && bracketSplit[i - 1] === '{') ||
-          bracketSplit[i - 1] === ';'
-        ) {
-          const removeEscapeChr = item.replace(/\n| |\t/g, '');
-          const isCssProperty =
-            cssKeyOfcssProperties[removeEscapeChr] ||
-            jsxKeyOfCssProperties[removeEscapeChr];
-          return isCssProperty ? (
-            <TagCode key={keyMaker(10) + item}>{item}</TagCode>
-          ) : (
-            <IncorrectTagCode key={keyMaker(10) + item}>
-              {item}
-            </IncorrectTagCode>
-          );
-        }
+      if (
+        (isInTag && bracketSplit[i - 1] === '{') ||
+        bracketSplit[i - 1] === ';'
+      ) {
+        const removeEscapeChr = item.replace(/\n| |\t/g, '');
+        const isCssProperty =
+          cssKeyOfcssProperties[removeEscapeChr] ||
+          jsxKeyOfCssProperties[removeEscapeChr];
+        return isCssProperty ? (
+          <TagCode key={keyMaker(10) + item}>{item}</TagCode>
+        ) : (
+          <IncorrectTagCode key={keyMaker(10) + item}>{item}</IncorrectTagCode>
+        );
+      }
 
-        if (isInTag && item === ':') {
-          return <Punctuati key={keyMaker(10) + item}>{item}</Punctuati>;
-        }
+      if (isInTag && item === ':') {
+        return <Punctuati key={keyMaker(10) + item}>{item}</Punctuati>;
+      }
 
-        if (isInTag && bracketSplit[i - 1] === ':') {
-          return bracketSplit[i + 1] === ';' ? (
-            <AttributeName key={keyMaker(10) + item}>{item}</AttributeName>
-          ) : (
-            <InCorrectAttributeName key={keyMaker(10) + item}>
-              {item}
-            </InCorrectAttributeName>
-          );
-        }
+      if (isInTag && bracketSplit[i - 1] === ':') {
+        return bracketSplit[i + 1] === ';' ? (
+          <AttributeName key={keyMaker(10) + item}>{item}</AttributeName>
+        ) : (
+          <InCorrectAttributeName key={keyMaker(10) + item}>
+            {item}
+          </InCorrectAttributeName>
+        );
+      }
 
-        if (item === '}') {
-          isInTag = false;
-          return <Punctuati key={keyMaker(10) + item}>{item}</Punctuati>;
-        }
+      if (item === '}') {
+        isInTag = false;
+        return <Punctuati key={keyMaker(10) + item}>{item}</Punctuati>;
+      }
 
-        return item;
-      });
+      return item;
+    });
 
-      highlightElement.current = element;
-    },
-    [cssCode],
-  );
+    highlightElement.current = element;
+    setRender(element);
+  }, []);
+
+  useEffect(() => {
+    query(cssCode);
+  }, []);
 
   return [copyCssCode.current, highlightElement.current, query];
 }
