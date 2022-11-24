b814164ded6abd9b9859b61bd56a114002292fa8
임현정 | Alex Im <84281505+h-alex2@users.noreply.github.com>
2022-11-24 19:24:13 +0900

    Feat cookiesprovider 추가

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { CookiesProvider } from 'react-cookie';
 import App from './components/App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+root.render(
+  <CookiesProvider>
+    <App />
+  </CookiesProvider>,
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
