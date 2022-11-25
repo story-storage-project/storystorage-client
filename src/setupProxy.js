0306b01382ab0fbfe660d5cbfc875a2e2a3a0ac3
임현정 | Alex Im <84281505+h-alex2@users.noreply.github.com>
2022-11-25 10:22:00 +0900

    Feat proxy 수정

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -2,14 +2,14 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = app => {
   app.use(
-    createProxyMiddleware(['/auth/google', '/auth/**'], {
+    createProxyMiddleware(['/auth/google'], {
       target: process.env.REACT_APP_BASE,
       changeOrigin: true,
       router: {
-        '/v2': process.env.REACT_APP_BASE,
+        '/auth/google': process.env.REACT_APP_BASE,
       },
       pathRewrite: {
-        '^/v2': '',
+        '^/auth/google': '/auth/google',
       },
     }),
   );
