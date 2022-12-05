d43c71f520766aa93c21a3fd0005514dc820a7c4
임현정 | Alex Im <84281505+h-alex2@users.noreply.github.com>
2022-12-05 22:13:07 +0900

    Feat test setup

---
+++ src/testUtil.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { render } from '@testing-library/react';
+import { ThemeProvider } from 'styled-components';
+import { CookiesProvider } from 'react-cookie';
+import { RecoilRoot } from 'recoil';
+import base, { lightTheme } from './components/theme/default';
+import GlobalStyle from './components/theme/GlobalStyle';
+
+const theme = {
+  ...base,
+  colors: lightTheme,
+};
+
+function AllTheProviders({ children }) {
+  return (
+    <CookiesProvider>
+      <RecoilRoot>
+        <ThemeProvider theme={theme}>
+          <GlobalStyle />
+          {children}
+        </ThemeProvider>
+      </RecoilRoot>
+    </CookiesProvider>
+  );
+}
+
+const customRender = (ui, options) => {
+  return render(ui, { wrapper: AllTheProviders, ...options });
+};
+
+export * from '@testing-library/react';
+
+export { customRender as render };
+
+AllTheProviders.propTypes = {
+  children: PropTypes.node.isRequired,
+};
